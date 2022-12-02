e3588608a992d490204fe217342b2e5a02425c42
Nicolas Cousin <contact@nicolas-cousin.com>
2022-12-02 22:01:36 +0000

    #3: Day 1

--- src/AdventOfCode/Program.cs
+++ src/AdventOfCode/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text;
+
+using AdventOfCode;
+
+var day1Input = System.IO.File.ReadAllLines("src/AdventOfCode/Day1.txt");
+
+var sumByElf = day1Input.SumByElf();
+
+var max = sumByElf.Max();
+
+var sum = sumByElf.OrderDescending().Take(3).Sum();
+
+StringBuilder sb = new();
+
+sb
+    .AppendLine($"The max is : {max}")
+    .AppendLine($"The top three elves sum is : {sum}");
+
+Console.WriteLine(sb.ToString());
