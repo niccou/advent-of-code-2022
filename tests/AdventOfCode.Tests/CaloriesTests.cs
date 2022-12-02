429aeee821bc6b9346986a479b57fee8dc34220f
Nicolas Cousin <contact@nicolas-cousin.com>
2022-12-02 22:01:36 +0000

    #3: Day 1

---
+++ tests/AdventOfCode.Tests/CaloriesTests.cs
@@ -0,0 +1,34 @@
+namespace AdventOfCode.Tests;
+
+public class UnitTest1
+{
+    private readonly List<string> _sample = new(){
+        "1000",
+        "2000",
+        "3000",
+        "",
+        "4000",
+        "",
+        "5000",
+        "6000",
+        "",
+        "7000",
+        "8000",
+        "9000",
+        "",
+        "10000",
+    };
+
+    [Fact]
+    public void SumCaloriesShouldReturnTheSumForEachElf()
+    {
+        List<int> expected = new()
+        {
+            6_000, 4_000, 11_000, 24_000, 10_000
+        };
+
+        List<int> sumByElves = _sample.SumByElf();
+
+        sumByElves.Should().BeEquivalentTo(expected);
+    }
+}
