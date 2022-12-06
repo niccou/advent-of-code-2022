360c0a6e850201dbfcf88677858beee14244b7bf
Nicolas Cousin <contact@nicolas-cousin.com>
2022-12-06 10:58:25 +0100

    Day6 (#14)

--- src/AdventOfCode/Program.cs
+++ src/AdventOfCode/Program.cs
@@ -3,7 +3,7 @@ using System.Text;
 
 using AdventOfCode;
 
-Day4();
+Day5();
 
 static void Day1()
 {
@@ -53,4 +53,34 @@ static void Day4()
 
     Console.WriteLine($"The count of fully contained assignments is {fullyContainedCount}");
     Console.WriteLine($"The count of overlap is {overlapCount}");
+}
+
+static void Day5()
+{
+    string[] input = { };
+
+    if (System.IO.File.Exists("src/AdventOfCode/Day5.txt"))
+    {
+        input = System.IO.File.ReadAllLines("src/AdventOfCode/Day5.txt");
+    }
+    else
+    {
+        input = System.IO.File.ReadAllLines("Day5.txt");
+    }
+
+    var cargo = input.GetStartingCrates();
+    var moves = input.GetMoves();
+
+    var topCrates = cargo.ProcessCrateMover9001Moves(moves).DisplayTopCrates();
+    Console.WriteLine($"The crates at the top are {topCrates}");
+}
+
+static void Day6()
+{
+    var input = System.IO.File.ReadAllText("src/AdventOfCode/Day6.txt");
+
+    var packetIndex = input.StartOfPacketIndex();
+    var messageIndex = input.StartOfMessageIndex();
+
+    Console.WriteLine($"The packet index is {packetIndex} and the message index is {messageIndex}");
 }
